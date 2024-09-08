0eca17eeeb7fbb6e4a63a151b930d4b66e3d38ba
João Bergallo <jhbergallo@gmail.com>
2024-09-07 23:30:40 -0300

    base button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { BaseButtonComponent } from './components/base-button/base-button.component';
-
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, BaseButtonComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
