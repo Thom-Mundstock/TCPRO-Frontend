de8df5291f5a6a3283977aacc048f17cb7a0e3aa
Thom-Mundstock <thomasmundstock@gmail.com>
2024-09-10 18:23:42 -0300

    Code review

--- src/app/components/label-projeto-status/label-projeto-status.component.ts
+++ src/app/components/label-projeto-status/label-projeto-status.component.ts
@@ -1,45 +1,44 @@
 import { Component, Input } from '@angular/core';
-import { TagComponent } from "./tag/tag.component";
+import { TagComponent } from './tag/tag.component';
 
-type projetoStatus = "EM_ANDAMENTO" | "FINALIZADO" | "BACKLOG" | "PLANEJAMENTO";
+type projetoStatus = 'EM_ANDAMENTO' | 'FINALIZADO' | 'BACKLOG' | 'PLANEJAMENTO';
 
 @Component({
   selector: 'app-label-projeto-status',
   standalone: true,
   imports: [TagComponent],
   templateUrl: './label-projeto-status.component.html',
-  styleUrl: './label-projeto-status.component.css'
+  styleUrl: './label-projeto-status.component.css',
 })
 export class LabelProjetoStatusComponent {
-    @Input({required: true}) tipo!: projetoStatus;
-    text!: string;
+  @Input({ required: true }) tipo!: projetoStatus;
+  text!: string;
+  color!: string;
 
-    color!: string;
+  ngOnInit() {
+    this.setLabelColor();
+    this.labelInit();
+  }
 
-    ngOnInit() {
-      this.setLabelColor();
-      this.labelInit();
-    }
+  setLabelColor() {
+    const labelTipoColor = {
+      EM_ANDAMENTO: '#FFB500',
+      FINALIZADO: '#7ED298',
+      BACKLOG: '#B1B2BE',
+      PLANEJAMENTO: '#95B8FC',
+    };
 
-    setLabelColor() {
-      const  labelTipoColor = {
-        "EM_ANDAMENTO": "#FFB500",
-        "FINALIZADO": "#7ED298",
-        "BACKLOG": "#B1B2BE",
-        "PLANEJAMENTO": "#95B8FC"
-      }
+    this.color = labelTipoColor[this.tipo as projetoStatus];
+  }
 
-      this.color = labelTipoColor[this.tipo as projetoStatus];
-    }
+  labelInit() {
+    const labelTipoText = {
+      EM_ANDAMENTO: 'Em andamento',
+      FINALIZADO: 'Finalizado',
+      BACKLOG: 'Backlog',
+      PLANEJAMENTO: 'Planejamento',
+    };
 
-    labelInit(){
-      const labelTipoText = {
-        "EM_ANDAMENTO": "Em andamento",
-        "FINALIZADO": "Finalizado",
-        "BACKLOG": "Backlog",
-        "PLANEJAMENTO": "Planejamento"
-      }
-
-      this.text = labelTipoText[this.tipo as projetoStatus];
-    }
+    this.text = labelTipoText[this.tipo as projetoStatus];
+  }
 }
