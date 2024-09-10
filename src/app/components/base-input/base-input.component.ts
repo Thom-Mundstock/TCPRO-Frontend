18e7550eaab4b48c00fb591554d3026aa6c386ef
leoramosjr <leonardo-ramos@uergs.edu.br>
2024-09-09 21:49:20 -0300

    feat: added system base input

---
+++ src/app/components/base-input/base-input.component.ts
@@ -0,0 +1,28 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-base-input',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './base-input.component.html',
+  styleUrl: './base-input.component.css'
+})
+export class BaseInputComponent {
+  @Input() id: string = '';
+  @Input() name: string = '';
+  @Input() value: any = '';
+  @Input() label: string = '';
+  @Input() placeholder: string = '';
+  @Input() type: string = 'text';
+  @Input() disabled: boolean = false;
+  @Input() required: boolean = false;
+  @Input() width: string = '100%';
+  @Output() valueChange = new EventEmitter<any>();
+
+  handleOnChange(event: any) {
+    this.valueChange.emit(event.target.value);
+  }
+  
+  constructor() {}
+}
