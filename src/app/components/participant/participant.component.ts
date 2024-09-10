07c4f400002dbeb2c4d10afe06ddc16665dcb5fd
marina-yamaguti <marina.geller.yamaguti@gmail.com>
2024-09-10 18:37:02 -0300

    feat: participant component and logic to display based on role

---
+++ src/app/components/participant/participant.component.ts
@@ -0,0 +1,34 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-participant',
+  standalone: true,
+  imports: [],
+  templateUrl: './participant.component.html',
+  styleUrl: './participant.component.css'
+})
+export class ParticipantComponent {
+  @Input() name: string = '';
+  @Input() role: string = '';
+
+  // método para retornar as iniciais do nome
+  getInitials(): string {
+    return this.name.split(' ')
+      .map((n) => n[0])
+      .join('')
+      .toUpperCase()
+      .slice(0, 2);  // retorna as primeiras duas letras
+  }
+
+  // método para retornar a cor do avatar
+  getCircleColor(): string {
+    switch (this.role.toLowerCase()) {
+      case 'gerente':
+        return '#FF7077'; 
+      case 'unidade envolvida':
+        return '#FFC6C8';
+      default:
+        return '#9696FF'; 
+    }
+  }
+}
