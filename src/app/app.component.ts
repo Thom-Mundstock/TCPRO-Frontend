207ab966f34711a35bd0bb5e90c4f413d7af1a1a
Thom-Mundstock <thomasmundstock@gmail.com>
2024-09-11 17:23:14 -0300

    Review Adjustments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BaseInputComponent } from './components/base-input/base-input.component';
+import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, BaseInputComponent],
+  imports: [RouterOutlet, BaseInputComponent, NavigationBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
