7aee63c48f6eefa2bbd3ba66e427cae95de9b160
João Bergallo <jhbergallo@gmail.com>
2024-09-19 18:48:08 -0300

    starting base popup

---
+++ src/app/components/base-popup/base-popup.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-base-popup',
+  standalone: true,
+  imports: [],
+  templateUrl: './base-popup.component.html',
+  styleUrl: './base-popup.component.css'
+})
+export class BasePopupComponent {
+
+}
