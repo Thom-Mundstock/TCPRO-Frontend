7bcda087d1cae712ad58a99f887c3486d44d4aad
Thom-Mundstock <thomasmundstock@gmail.com>
2024-09-19 18:32:50 -0300

    Fixing color bug

--- src/app/components/participant/participant.component.ts
+++ src/app/components/participant/participant.component.ts
@@ -11,6 +11,19 @@ export class ParticipantComponent {
   @Input() name: string = '';
   @Input() role: string = '';
 
+  getCircleColor(): string {
+    const rootStyles = getComputedStyle(document.documentElement);
+
+    switch (this.role.toLowerCase()) {
+      case 'gerente':
+        return rootStyles.getPropertyValue('--tertiary').trim();
+      case 'unidade envolvida':
+        return rootStyles.getPropertyValue('--inputFocus').trim();
+      default:
+        return rootStyles.getPropertyValue('--secondary').trim();
+    }
+  }
+
   // método para retornar as iniciais do nome
   getInitials(): string {
     return this.name
@@ -21,6 +34,7 @@ export class ParticipantComponent {
       .slice(0, 2); // retorna as primeiras duas letras
   }
 
+  /*
   // método para retornar a cor do avatar
   getCircleColor(): string {
     switch (this.role.toLowerCase()) {
@@ -31,5 +45,5 @@ export class ParticipantComponent {
       default:
         return '#9696FF';
     }
-  }
+  }*/
 }
